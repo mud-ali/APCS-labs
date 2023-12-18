7f38c9e41f519626d791e682f309de88adbb2145
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-12-18 11:08:40 -0500

    fix comment format

--- vehicle/Car.java
+++ vehicle/Car.java
@@ -7,10 +7,10 @@ public abstract class Car {
     private String model;
     private double miles;
 
-    /*
+    /**
      * Creates a car with starting total miles on the odometer.
      * @throws IllegalArgumentException if startingOdometerValue is negative
-    */
+     */
     public Car(String make, String model, double startingOdometerValue) {
         this.make = make;
         this.model = model;
@@ -24,31 +24,31 @@ public abstract class Car {
         this(make, model, 0.0);
     }
 
-    /*
-    * If able to drive the full given number of miles, returns true. If
-    * not, returns false. (Determination is based upon how far the car can
-    * drive given the remaining fuel/energy reserves.)
-    * @throws IllegalArgumentException if miles is negative.
-    */
+    /**
+     * If able to drive the full given number of miles, returns true. If
+     * not, returns false. (Determination is based upon how far the car can
+     * drive given the remaining fuel/energy reserves.)
+     * @throws IllegalArgumentException if miles is negative.
+     */
     public boolean canDrive(double miles) {
         if (miles < 0)
             throw new IllegalArgumentException("Miles cannot be a negative value");
         return miles <= getRemainingRange();
     }
 
-    /*
-    * Drives the full given number of miles.
-    * @throws IllegalArgumentException if miles is negative or if miles is
-    * too high given the current fuel.
-    */
+    /**
+     * Drives the full given number of miles.
+     * @throws IllegalArgumentException if miles is negative or if miles is
+     * too high given the current fuel.
+     */
     public abstract void drive(double miles);
 
-    /*
-    * Gives String representation of Car as
-    * "<make and model> (<miles> mi)"
-    * Odometer miles should be rounded to 1 decimal place. If miles is a
-    * whole number, ".0" should still display.
-    */
+    /**
+     * Gives String representation of Car as
+     * "<make and model> (<miles> mi)"
+     * Odometer miles should be rounded to 1 decimal place. If miles is a
+     * whole number, ".0" should still display.
+     */
     public String toString() {
         return String.format("%s %s (%0.2f mi)", make, model, getOdometerMiles());
     }
@@ -68,29 +68,29 @@ public abstract class Car {
         return this.model;
     }
 
-    /*
-    * Returns how many more miles the car can currently go given the
-    * remaining fuel/energy reserves.
-    */
+    /**
+     * Returns how many more miles the car can currently go given the
+     * remaining fuel/energy reserves.
+     */
     public abstract double getRemainingRange();
 
-    /*
-    * Adds miles to the odometer.
-    * @throws IllegalArgumentException if miles is negative.
-    */
+    /**
+     * Adds miles to the odometer.
+     * @throws IllegalArgumentException if miles is negative.
+     */
     protected void addMiles(double miles) {
         this.miles += miles;
     }
 
-    /*
-    * The car attempts to drive, in order, each of the daily number of
-    * miles in the list milesEachDay. Once the car cannot drive one of the
-    * day’s distance, no more days are attempted. Returns the number of
-    * days successfully driven.
-    * @throws IllegalArgumentException if miles is negative for any of the
-    * attempted days. The exception check should occur prior to any driving
-    * is attempted.
-    */
+    /**
+     * The car attempts to drive, in order, each of the daily number of
+     * miles in the list milesEachDay. Once the car cannot drive one of the
+     * day’s distance, no more days are attempted. Returns the number of
+     * days successfully driven.
+     * @throws IllegalArgumentException if miles is negative for any of the
+     * attempted days. The exception check should occur prior to any driving
+     * is attempted.
+     */
     public int roadTrip(List<Double> milesEachDay) {
         for (int i=0;i<milesEachDay.size();i++) {
             if (milesEachDay.get(i) < 0)
