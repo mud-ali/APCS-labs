499e175c3804e3af3d749f7dccc50f988b5a29d7
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-12-18 10:46:31 -0500

    implement flying cars

--- vehicle/Flying.java
+++ vehicle/Flying.java
@@ -1,4 +1,12 @@
 package vehicle;
+
 public interface Flying {
-    
+    /** @throws IllegalArgumentException if miles is negative. */
+    public boolean canFly(double miles);
+    /**
+     * @throws IllegalArgumentException if miles is negative.
+     * @throws IllegalArgumentException if miles exceeds the remaining
+     * range of the car.
+     * */
+    public void fly(double miles);
 }
