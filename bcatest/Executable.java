7235a64640e0c82569d8240c125e86e412ade58c
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-12-19 10:21:28 -0500

    remove tests

--- bcatest/Executable.java
+++
@@ -1,9 +0,0 @@
-package bcatest;
-
-/** Source JUnit Executable implementation
- * Simplified for our introductory testing purposes. */
-
-@FunctionalInterface
-public interface Executable {
-    void execute() throws Throwable;
-}
